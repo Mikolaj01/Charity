17ac47d17efef51bc383ddf2f9bc7f532b8a0846
Mikolaj01 <66174124+Mikolaj01@users.noreply.github.com>
2021-02-18 15:22:45 +0000

    initial charity files

---
+++ booking.js
@@ -0,0 +1,8 @@
+// Only when the box is checked will the alert show.
+function book() {
+  var checkBox = document.getElementById("check");
+
+  if (checkBox.checked == true) {
+    alert("Thank you for booking");
+  }
+}
